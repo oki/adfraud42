832b4e0dbb37f861860160c7722dcb270a63dee8
Michał Kurek <oki@md6.org>
2012-06-04 19:27:58 +0200

    Init commit, clean app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adfraud42::Application.config.secret_token = 'a938b4aad36e98d42a2b5e57c38f2ec0730eb785d420e7fa9ee820a413c3022407ce86a3b77815eacaf43995d767d14d5a4d744853e2198e28f315aa5fef6fd3'
